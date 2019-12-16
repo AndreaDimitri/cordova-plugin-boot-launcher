f12760c6e3acf24945520b0d7db514d9ec53a6f0
AndreaDimitri <58943091+AndreaDimitri@users.noreply.github.com>
2019-12-16 15:14:34 +0100

    Update BootLauncher.java

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -10,7 +10,7 @@ public class BootLauncher extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent serviceIntent = new Intent(context, com.example.parentalcontrol.class);
+        Intent serviceIntent = new Intent(context, com.example.parentalcontrol.ParentalControl.class);
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
